c9909f5b8d96a6d601ccf3e3c0d0415c7246f102
unknown <luisbayro1813@gmail.com>
2026-04-24 00:48:19 -0600

    primer despliegue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//hola mundo
 app.get('/', (req, res) => {
   res.send('<h1>ERP Demo: Despliegue Seguro ✅</h1><p>Estado: Funcionando en contenedor.</p>');
 });
